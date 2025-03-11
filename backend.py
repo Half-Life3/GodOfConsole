c7ee200f6e0298299f8b102619abc3df2adc1745
Half Life 3 <olsky7@yandex.ru>
2025-03-12 02:10:00 +0300

    new era of goc

--- backend.py
+++ backend.py
@@ -3,6 +3,9 @@ import os
 import sys
 import shutil
 import re
+from dataclasses import dataclass
+from logging import raiseExceptions
+from subprocess import check_call
 
 
 def prepare_env():
@@ -12,7 +15,7 @@ def prepare_env():
         os.mkdir(f"{os.getcwd()}/experiment_directory/")
         with open(f"{os.getcwd()}/experiment_directory/test.txt", 'w') as file:
             file.write('Hello, world!')
-        return print('Каталог Experiment для создан')
+        return print('Каталог Experiment создан')
 
 
 def clear_env():
@@ -21,38 +24,75 @@ def clear_env():
 
 
 class GodOfConsole:
-    workdirectory = None
-    targetdirectory = None
-    targetfile = None
+    _workdirectory: str = os.getcwd()
+    _destination: str
+    _filename: str
     filemask = r'^[a-zA-Z0-9_]+\.[a-zA-Z0-9]+$'
 
-    def check_data_format(self, data: str):
-        """Хочу знать что за данные вводит пользователь"""
-        if os.path.isfile(data):
-            return "Это файл"
-        elif os.path.isdir(data):
-            return "Это папка"
+    @classmethod
+    def get_workdirectory(cls):
+        return cls._workdirectory
+
+    @classmethod
+    def set_workdirectory(cls, value):
+        if os.path.exists(value):
+            cls._workdirectory = value
         else:
-            return "Это не файл и не папка"
+            print('Путь не существует')
 
-    def check_filename_format(self, filename: str):
+    @classmethod
+    def get_destination(cls):
+        return cls._destination
+
+    @classmethod
+    def set_destination(cls, value):
+        if value == None:
+            cls._destination = cls.get_workdirectory()
+        elif os.path.exists(value):
+            cls._destination = value
+        else:
+            print('Путь не существует')
+
+    @classmethod
+    def get_filename(cls):
+        return cls._filename
+
+    @classmethod
+    def set_filename(cls, value):
+        # если полный путь с именем
+        if os.path.isfile(value):
+            cls._filename = os.path.basename(value)
+        cls._filename = value
+
+    @classmethod
+    def get_abs_path_for_file(cls, data: str):
+        """Хочу знать что за данные вводит пользователь"""
+        if cls.check_filename_format(data) and os.path.isfile(data):
+            return os.path.join(cls.get_workdirectory(), data)
+        else:
+            raise TypeError('Это не файл')
+
+    @classmethod
+    def check_filename_format(cls, filename: str) -> bool:
         """Регулярное выражение для проверки формата name.format"""
-        return bool(re.match(self.filemask, filename))
+        return bool(re.match(cls.filemask, filename))
 
-    def is_absolute_path(self, path: str):
+    def check_exist_file(cls, data: str) -> bool:
+
+        return os.path.exists(data)
+
+    @classmethod
+    def is_absolute_path(cls, path: str):
         """Method from os doc"""
         return os.path.isabs(path)
 
-    def get_filename_from_path(self, absolute_path: str):
+    @classmethod
+    def get_filename_from_path(cls, absolute_path: str):
         """Method from os doc"""
-        if self.is_absolute_path(absolute_path):
+        if cls.is_absolute_path(absolute_path):
             return os.path.basename(absolute_path)
 
-    def getworkdirectory(self):
-        self.workdirectory = os.getcwd()
-        return self.workdirectory
-
-    def delete_filled_dir(self, top: str) -> bool:
+    def delete_filled_dir(cls, top: str) -> bool:
         """Method from os doc"""
         for root, dirs, files in os.walk(top, topdown=False):
             for name in files:
@@ -62,34 +102,40 @@ class GodOfConsole:
         os.rmdir(top)
         return True
 
-    def copy_file(self, path: str) -> bool:
+    @classmethod
+    def copy_file(cls, userdata: str, destination_path: str = None):
         """(легкое) команда, которая позволяет копировать файл(пример использования: manager copy test.txt"""
-        if self.check_data_format(path) == 'Это файл':
-            shutil.copyfile(path, self.getworkdirectory())
-            print(f'Файл "{self.get_filename_from_path(path)}" успешно скопирован в "{self.workdirectory}"')
+        cls.set_filename(userdata)
+        cls.set_destination(destination_path)
+        file_name_without_extension, file_extension = os.path.splitext(cls.get_filename())
+        new_file_name = file_name_without_extension + "_copy" + file_extension
+        shutil.copyfile(os.path.join(cls.get_workdirectory(), cls.get_filename()),
+                        os.path.join(cls.get_destination(), new_file_name))
+        return print(
+            f'Файл "{cls.get_filename()}" успешно скопирован в "{cls.get_destination()}"')
 
-        else:
-            print('Error in copy method')
-
-    def delete_file_or_dir(self, path: str):
+    @classmethod
+    def delete_file_or_dir(cls, userdata: str):
         """команда, которая удаляет файл или папку(пример использования: manager delete folder_name)"""
-        if os.path.isfile(path):
-            os.remove(path)
-            print(f"Файл '{path}' был удален")
-        elif os.path.isdir(path):
-            shutil.rmtree(path)
-            print(f"Папка '{path}' была удалена")
+        if os.path.isfile(userdata):
+            os.remove(userdata)
+            print(f"Файл '{userdata}' был удален")
+        elif os.path.isdir(userdata):
+            shutil.rmtree(userdata)
+            print(f"Папка '{userdata}' была удалена")
         else:
-            print(f"'{path}' не является ни файлом, ни папкой")
+            print(f"'{userdata}' - невалидные данные")
 
-    def filecount_in_dir(self, path: str) -> int:
+    @classmethod
+    def filecount_in_dir(cls, path: str):
         """(среднее) команда, подсчитывающая количество файлов в папке (в том числе вложенные"""
         file_count = 0
         for root, dirs, files in os.walk(path):
             file_count += len(files)
-        return file_count
+        return print(file_count)
 
-    def find_files(self, path: str, mask: str):
+    @classmethod
+    def find_files(cls, path: str, mask: str):
         """(среднее) команда, ищущая все подходящие файлы в папке (в том числе
     вложенные) по фильтру (например, через регулярное выражение. Будет
     полезно почитать про библиотеку re)"""
@@ -99,48 +145,63 @@ class GodOfConsole:
         for root, dirs, files in os.walk(path):
             for file in files:
                 if regex.search(file):
+                    print(f'{root}{file}')
                     matching_files.append(os.path.join(root, file))
 
-        return matching_files
+        return print(matching_files)
 
-    def rename_file(self, path: str):
+    @classmethod
+    def rename(cls, path: str, recursive: bool):
         """(среднее) команда, добавляющая в название файла дату его создания (если
     выбрана папка - во все файлы в папке, если есть ключ --recursive - во все
     файлы даже на нескольких уровнях вложения, метаданные файла можно
     получить например через библиотеку os)"""
-        if self.check_data_format(path) == 'Это файл':
-            # Получаем время создания файла
-            creation_time = os.path.getctime(path)
-            creation_date = datetime.datetime.fromtimestamp(creation_time).strftime('%Y-%m-%d')
-
-            # Получаем директорию и имя файла
-            directory, filename = os.path.split(path)
-
-            # Создаем новое имя файла
-            new_filename = f"{creation_date}_{filename}"
-            new_path = os.path.join(directory, new_filename)
-
-            # Переименовываем файл
-            os.rename(path, new_path)
-            return print(f"Файл '{path}' переименован в: '{new_path}'")
-        else:
-            return print('Error')
-
-    def rename_files_in_dir(self, path: str, recursive: bool):
-        if self.check_data_format(path) == 'Это папка':
+        if os.path.isfile(path):
+            cls._renamefile(path)
+        elif os.path.isdir(path):
             for root, dirs, files in os.walk(path):
                 for file in files:
                     file_path = os.path.join(root, file)
-                    self.rename_file(file_path)
+                    cls._renamefile(file_path)
                 # Если не рекурсивный, выходим из цикла после первого уровня
                 if not recursive:
                     break
         else:
             return print('Error')
 
-    def analyse_workdir(self, path):
+    @classmethod
+    def _renamefile(cls, path: str):
+        """(среднее) команда, добавляющая в название файла дату его создания (если
+    выбрана папка - во все файлы в папке, если есть ключ --recursive - во все
+    файлы даже на нескольких уровнях вложения, метаданные файла можно
+    получить например через библиотеку os)"""
+        # Получаем время создания файла
+        creation_time = os.path.getctime(path)
+        creation_date = datetime.datetime.fromtimestamp(creation_time).strftime('%Y-%m-%d')
+
+        # Получаем директорию и имя файла
+        directory, filename = os.path.split(path)
+
+        # Создаем новое имя файла
+        new_filename = f"{creation_date}_{filename}"
+        new_path = os.path.join(directory, new_filename)
+
+        # Переименовываем файл
+        os.rename(path, new_path)
+        return print(f"Файл '{path}' переименован в: '{new_path}'")
+
+    @classmethod
+    def analyse_workdir(cls, path: str, size_limit: int):
         """(сложное) команда, запускающая анализ всех вложенных папок и файлов, и
     выводящая информацию о том, насколько большие файлы находятся на уровне
     вызова. Способ вывода любой (но только через консоль), например:
     manager analyse"""
-        pass
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                try:
+                    file_size = os.path.getsize(file_path)
+                    if file_size > size_limit:
+                        print(f"Файл: {file_path}, Размер: {file_size} байт")
+                except Exception as e:
+                    print(f"Не удалось получить размер файла {file_path}: {e}")
--- frontend.py
+++ frontend.py
@@ -1,43 +1,55 @@
 import argparse
 from backend import GodOfConsole
 
-god = GodOfConsole()
-parser = argparse.ArgumentParser('Simple file manager')
+parser = argparse.ArgumentParser('Simple file manager. Require to use abs path')
 
 subparser = parser.add_subparsers(dest='command', required=True)
 
 #copy
 parser_copy = subparser.add_parser('copy', help='Copy file in workdirectory')
-parser_copy.add_argument('path', type=str, help='absolute path to file or filename in workdirectory')
-parser_copy.set_defaults(func=god.copy_file)
+parser_copy.add_argument('userdata', type=str, help='absolute path to file or local filename.format')
+parser_copy.add_argument('--destination', type=str, help='destination path for files copy or local', default=None, required=False)
+parser_copy.set_defaults(func=GodOfConsole.copy_file)
 
 #delete
 parser_delete = subparser.add_parser('delete', help='Delete file or all files in directory')
-parser_delete.add_argument('path', type=str, help='file value')
-parser_delete.set_defaults(func=god.delete_file_or_dir)
+parser_delete.add_argument('userdata', type=str, help='file name or directory')
+parser_delete.set_defaults(func=GodOfConsole.delete_file_or_dir)
 
 #filecount_in_dir
-parser_filecount_in_dir = subparser.add_parser('filecount_in_dir', help='Return count files in directory')
+parser_filecount_in_dir = subparser.add_parser('filecount', help='Return count files in directory')
 parser_filecount_in_dir.add_argument('path', type=str, help='absolute path or dirname in workdirectory')
-parser_filecount_in_dir.set_defaults(func=god.filecount_in_dir)
+parser_filecount_in_dir.set_defaults(func=GodOfConsole.filecount_in_dir)
 
 #find_files
-parser_find_files = subparser.add_parser('find_files', help='Find files in directory with mask')
-parser_find_files.add_argument('path', type=str, help='file value')
-parser_find_files.add_argument('mask', type=str, help='file value')
-parser_find_files.set_defaults(func=god.find_files)
+parser_find_files = subparser.add_parser('find', help='Find files in directory with mask')
+parser_find_files.add_argument('path', type=str, help='abs file path')
+parser_find_files.add_argument('mask', type=str, help='mask value')
+parser_find_files.set_defaults(func=GodOfConsole.find_files)
 
 #rename_file
-parser_rename_file = subparser.add_parser('rename_file', help='Renaming a file with the date the file was created')
-parser_rename_file.add_argument('path', type=str, help='file value')
-parser_rename_file.set_defaults(func=god.rename_file)
+parser_rename_file = subparser.add_parser('rename', help='Renaming a file with the date the file was created')
+parser_rename_file.add_argument('path', type=str, help='abs file path')
+parser_rename_file.add_argument('--recursive', help='recursive flag', type=str, required=False, default=False)
+parser_rename_file.set_defaults(func=GodOfConsole.rename)
 
-#rename_files_in_dir
-parser_rename_files_in_dir = subparser.add_parser('rename_files_in_dir', help='Renaming a files in dir with the date the file was created')
-parser_rename_files_in_dir.add_argument('path', type=str, help='file value')
-parser_rename_files_in_dir.set_defaults(func=god.rename_files_in_dir)
-
+#analyse_workdir
+parser_rename_file = subparser.add_parser('analyse', help='Analysis of all subfolders and files')
+parser_rename_file.add_argument('path', type=str, help='abs path')
+parser_rename_file.add_argument('size_limit', help='Size in bytes for filtering large files', type=int)
+parser_rename_file.set_defaults(func=GodOfConsole.analyse_workdir)
 
 args = parser.parse_args()
 
-args.func(args.path)
+if args.command == 'copy':
+    args.func(args.userdata, args.destination)
+if args.command == 'delete':
+    args.func(args.userdata)
+if args.command == 'filecount':
+    args.func(args.path)
+if args.command == 'find':
+    args.func(args.path, args.mask)
+if args.command == 'rename':
+    args.func(args.path, args.recursive)
+if args.command == 'analyse':
+    args.func(args.path, args.size_limit)
